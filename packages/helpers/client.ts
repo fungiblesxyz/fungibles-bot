e3cb953f87df9b9fc14d6d6dcd4d3b1961db3383
Sam <samuv.eth@gmail.com>
2024-12-04 09:22:58 +0700

    update rpc

--- packages/helpers/client.ts
+++ packages/helpers/client.ts
@@ -3,7 +3,9 @@ import { base } from "viem/chains";
 
 const client = createPublicClient({
   chain: base,
-  transport: http("https://base.llamarpc.com"),
+  transport: http(
+    "https://rpc.ankr.com/base/a889495c40c52cceed37f260717b69733a0096a76216e1b8817651ab0d9aac94"
+  ),
 }) as PublicClient;
 
 export default client;
